19cabe66526f6fcc710c262ca6d9739f09f6d4e3
Sehyo Chang <sehyo@fluvio.io>
2025-01-27 13:13:07 -0800

    add ci and fix clippy

--- crates/fluvio-ws-proxy/src/main.rs
+++ crates/fluvio-ws-proxy/src/main.rs
@@ -17,6 +17,6 @@ async fn main() -> anyhow::Result<()> {
     fluvio_future::subscriber::init_tracer(None);
 
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), WS_PORT);
-    let _ws = start_web_services(addr).await?;
+    start_web_services(addr).await?;
     Ok(())
 }
